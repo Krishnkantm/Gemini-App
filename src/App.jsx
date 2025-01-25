5d22ef8b9b4ba583bdf0b1c5033f588f5f618c43
Krishnkant modi <modikishu41@gmail.com>
2025-01-25 15:19:28 +0530

    App-set

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
-// export default App
-
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
-import Main from '../components/Main/Main.jsx'
+import Main from './components/Main/Main'
+
 
 const App = () => {
   return (
