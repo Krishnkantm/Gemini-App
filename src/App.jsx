911a8bff7bb94434e6bb76a1ed3b4a3e86eb17f0
Krishnkant modi <modikishu41@gmail.com>
2025-01-25 14:57:44 +0530

    App-setup

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@
 
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
-import Main from './components/Main/main'
+import Main from '../components/Main/Main'
 
 const App = () => {
   return (
